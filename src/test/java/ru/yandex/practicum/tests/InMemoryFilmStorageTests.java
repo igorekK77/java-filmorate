ed56b17d66942af3620f003a26931f64ea2212e4
Igor <igorek.kopilov@yandex.ru>
2025-04-03 23:40:19 +0300

    Исправить тесты

--- src/test/java/ru/yandex/practicum/tests/InMemoryFilmStorageTests.java
+++ src/test/java/ru/yandex/practicum/tests/InMemoryFilmStorageTests.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.dto.GenreName;
+import ru.yandex.practicum.filmorate.dto.RatingName;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
 @Component
@@ -18,21 +21,24 @@ public class InMemoryFilmStorageTests {
 
     private static FilmStorage filmStorage;
     private static Film film;
+    private static GenreName genreName = new GenreName(1, "Test");
+    private static final RatingName ratingName = new RatingName(1, "Test");
 
 
     @BeforeEach
     public void beforeEach() {
         filmStorage = new InMemoryFilmStorage();
         film = new Film("testFilm", "dTestFilm", LocalDate.of(2015,11,23),
-                112, Set.of(Long.valueOf("2")), "PG-13");
+                112, List.of(genreName), ratingName);
+        genreName = new GenreName();
     }
 
     @Test
     public void testCreateFilm() {
         filmStorage.create(film);
         Film checkFilm = new Film("testFilm", "dTestFilm",
-                LocalDate.of(2015,11,23), 112, Set.of(Long.valueOf("2")),
-                "PG-13");
+                LocalDate.of(2015,11,23), 112, List.of(genreName),
+                ratingName);
         checkFilm.setId(1L);
         Assertions.assertEquals(checkFilm, film);
     }
@@ -42,7 +48,7 @@ public class InMemoryFilmStorageTests {
         filmStorage.create(film);
         Film updateFilm = new Film("testUpdateFilm", "dTestUpdateFilm",
                 LocalDate.of(2015,11,23), 112,
-                Set.of(Long.valueOf("2")), "PG-13");
+                List.of(genreName), ratingName);
         updateFilm.setId(1L);
         filmStorage.update(updateFilm);
         Assertions.assertEquals(updateFilm, film);
@@ -51,7 +57,7 @@ public class InMemoryFilmStorageTests {
     @Test
     public void testCreateFilmWithNullName() {
         Film nullNameFilm = new Film(null, "testFilm", LocalDate.of(2015,11,23),
-                112, Set.of(Long.valueOf("2")), "PG-13");
+                112, List.of(genreName), ratingName);
         Assertions.assertThrows(ValidationException.class, () -> filmStorage.create(nullNameFilm));
     }
 
@@ -59,19 +65,19 @@ public class InMemoryFilmStorageTests {
     public void testCreateFilmWithDescription201Characters() {
         Film testFilm = new Film("Test", "a".repeat(201),
                 LocalDate.of(2015,11,23), 112,
-                Set.of(Long.valueOf("2")), "PG-13");
+                List.of(genreName), ratingName);
         Assertions.assertThrows(ValidationException.class, () -> filmStorage.create(testFilm));
     }
 
     @Test
     public void testCreateFilmWithDescription200Characters() {
         Film testFilm = new Film("Test", "a".repeat(200),
-                LocalDate.of(2015,11,23), 112, Set.of(Long.valueOf("2")),
-                "PG-13");
+                LocalDate.of(2015,11,23), 112, List.of(genreName),
+                ratingName);
         filmStorage.create(testFilm);
         Film checkFilm = new Film("Test", "a".repeat(200),
                 LocalDate.of(2015,11,23), 112,
-                Set.of(Long.valueOf("2")), "PG-13");
+                List.of(genreName), ratingName);
         checkFilm.setId(1L);
         Assertions.assertEquals(checkFilm, testFilm);
     }
@@ -80,11 +86,11 @@ public class InMemoryFilmStorageTests {
     public void testCreateFilmWithDescription199Characters() {
         Film testFilm = new Film("Test", "a".repeat(199),
                 LocalDate.of(2015,11,23), 112,
-                Set.of(Long.valueOf("2")), "PG-13");
+                List.of(genreName), ratingName);
         filmStorage.create(testFilm);
         Film checkFilm = new Film("Test", "a".repeat(199),
                 LocalDate.of(2015,11,23), 112,
-                Set.of(Long.valueOf("2")), "PG-13");
+                List.of(genreName), ratingName);
         checkFilm.setId(1L);
         Assertions.assertEquals(checkFilm, testFilm);
     }
